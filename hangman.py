a302ba19b809d2e40670f84ce64559e6d9ba8e8c
Omar Mohamad Abou Ghoche <omarghoucher@gmail.com>
2020-10-29 16:30:11 -0300

    update HANGMAN GAME - better "GUI"

--- hangman.py
+++ hangman.py
@@ -1,71 +1,85 @@
+from getpass import getpass  # to input user text without writing it to the console
 from random import choice
 from sys import exit  # so I can close the program when needed
+from time import sleep  # to control program execution
 
-print("\n \t\t----------------")
-print(*' \t\tHANGMAN', sep=' ')
-print(" \t\t----------------\n")
+
+print("\n\t\t----------------")
+print(*'\t\tHANGMAN', sep=' ')
+print("\t\t----------------\n")
 
 while 1:
-    # Hangman ascii art by chrishorton (GitHub)
+    # adapted from Hangman ascii art by chrishorton (GitHub)
     hangmanpics = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
+                                          +---+
+                                              |
+                                              |
+                                              |
+                                              |
+                                              |
+                                        =========''', ''' 
+                                          +---+
+                                          |   |
+                                              |
+                                              |
+                                              |
+                                              |
+                                        =========''', '''
+                                          +---+
+                                          |   |
+                                          O   |
+                                              |
+                                              |
+                                              |
+                                        =========''', '''
+                                          +---+
+                                          |   |
+                                          O   |
+                                          |   |
+                                              |
+                                              |
+                                        =========''', '''
+                                          +---+
+                                          |   |
+                                          O   |
+                                         /|   |
+                                              |
+                                              |
+                                        =========''', '''
+                                          +---+
+                                          |   |
+                                          O   |
+                                         /|\  |
+                                              |
+                                              |
+                                        =========''', '''
+                                          +---+
+                                          |   |
+                                          O   |
+                                         /|\  |
+                                         /    |
+                                              |
+                                        =========''', '''
+                                          +---+
+                                          |   |
+                                          O   |
+                                         /|\  |
+                                         / \  |
+                                              |
+                                        =========''']
 
     menu = input('Type "play" to play the game, "exit" to quit: ')
 
     if menu == "play":        
     
-        word = input("\nHanger! Choose a word to be guessed (lower case): ")
-
-        print("\nNow pass the device to the poor hanged man/woman")
-        print("Let's see if he/she can guess your word!")
+        word = ""
+        while word == "":
+            word = getpass("\nHanger! Choose a word to be guessed (lower case | no spaces): ")
+        
+        print("\n\n\nNow pass the device to the poor hanged man/woman", flush=True)
+        sleep(2)
+        print("\nLet's see if he/she can guess your word!", flush=True)
+        sleep(2)
         
         right_basket = set()  # for letters in `word`
         wrong_basket = set()
@@ -75,54 +89,58 @@ while 1:
             """ This function checkes if the `char` is btwn 97 and 122 
                 in ASCII code, tat is, if `char` is a, b, c .... or z"""
             return all(97 <= ord(c) <= 122 for c in s)
-
-        print("\nYou got 6 chances to guess wrong!")
+        print("\nYou got 7 chances to guess wrong!")
         
         i = 0
-        while i < 6:  # user has 6 lives!
+        while i < 7:  # user has 7 lives!
             
+            sleep(2)
             # If basket and set(word) have same length, then user guessed all letters!
             if len(right_basket) != len(set(word)):
 
                 print()
                 # '-' is added to `hint` if word's letter is not found
                 hint = [c if c in right_basket else '-' for c in word]
-                print(''.join(hint))
+                print(hangmanpics[i])
+                print(f"\t\t\t\t{''.join(hint)}")
 
-                ltr = input("Input a letter: ")
+                ltr = input("\t\t\t\tInput a letter: ")
                 
                 # handling user typos
                 if len(ltr) != 1:
-                    print("You should input a single letter")
+                    print("\n\t\t\t\tYou should input a single letter")
                     continue
 
                 if is_ascii(ltr):
 
                     if ltr in word:
                         if ltr in right_basket:
-                            print("You already typed this letter")
+                            print("\n\t\t\t\tYou already typed this letter...")
                         else:
                             right_basket.add(ltr)
+                            print(f"\n\t\t\t\tYes! The word has the letter '{ltr}'!")
                     else:
                         if ltr in wrong_basket:
-                            print("You already typed this letter")
+                            print("\n\t\t\t\tYou already typed this letter...")
                         else:
                             wrong_basket.add(ltr)
-                            print("No such letter in the word")
+                            print("\n\t\t\t\tNo such letter in the word")
                             # user only get hanged if letter is not in word!
                             i += 1
-                            print(hangmanpics[i])
                 else:
-                    print("It is not an ASCII lowercase letter")
+                    print("\n\t\t\t\tThat's not an English lowercase letter!")
             else:
                 # if user guesses word:
                 print()
                 print(word)
-                print("You guessed the word!")
-                print("You survived!")
+                print("\n\t\t\t\tYou guessed the word!")
+                print("\t\t\t\tYou survived!")
                 exit(0)
-        # this block executes when all chances (6) are used!
-        print("You lost!")
+        # this block executes when all chances (7) are used!
+        sleep(1)
+        print("\n\t\t\t\tYou died!", flush=True)
+        print("\n\t\t\t\tAt least you had no chance to find out that you are a LOSER!!", flush=True)
+        sleep(2)
         print()
 
     elif menu == "exit":
